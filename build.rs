d48638fec26ea03c1f8bc3e40b23e79b4a5cf1c9
meh <meh@schizofreni.co>
2016-11-26 19:43:20 +0100

    crate: add static feature

--- build.rs
+++ build.rs
@@ -3,35 +3,35 @@ use std::env;
 extern crate pkg_config;
 use pkg_config::{Config, Error};
 
+fn is_static() -> bool {
+	env::var("CARGO_FEATURE_STATIC").is_ok()
+}
+
 fn cairo() -> Result<(), Error> {
 	if let Ok(path) = env::var("GTK_LIB_DIR") {
 		for lib in &["cairo"] {
-			println!("cargo:rustc-link-lib=dylib={}", lib);
+			println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
 		}
 
 		println!("cargo:rustc-link-search=native={}", path);
-
 		return Ok(());
 	}
 
-	Config::new().probe("cairo")?;
-
+	Config::new().statik(is_static()).probe("cairo")?;
 	Ok(())
 }
 
 fn pango() -> Result<(), Error> {
 	if let Ok(path) = env::var("GTK_LIB_DIR") {
 		for lib in &["pangocairo-1.0", "pango-1.0", "gobject-2.0", "glib-2.0"] {
-			println!("cargo:rustc-link-lib=dylib={}", lib);
+			println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
 		}
 
 		println!("cargo:rustc-link-search=native={}", path);
-
 		return Ok(());
 	}
 
-	Config::new().probe("pangocairo")?;
-
+	Config::new().statik(is_static()).probe("pangocairo")?;
 	Ok(())
 }
 
