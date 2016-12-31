5001518240aca50c3b338067d4bf8ccb90d9c305
meh <meh@schizofreni.co>
2016-12-31 20:56:10 +0100

    terminal/sixel: minor optimizations

--- src/terminal/sixel.rs
+++ src/terminal/sixel.rs
@@ -35,7 +35,7 @@ pub struct Sixel {
 	position: (u32, u32),
 
 	colors:     HashMap<u32, (u8, u8, u8, u8), BuildHasherDefault<FnvHasher>>,
-	color:      u32,
+	color:      (u8, u8, u8, u8),
 	background: (u8, u8, u8, u8),
 }
 
@@ -51,7 +51,7 @@ impl Sixel {
 			position: (0, 0),
 
 			colors:     Default::default(),
-			color:      0,
+			color:      (0, 0, 0, 255),
 			background: (
 				(background.red   * 255.0) as u8,
 				(background.green * 255.0) as u8,
@@ -77,7 +77,7 @@ impl Sixel {
 	}
 
 	pub fn enable(&mut self, id: u32) {
-		self.color = id;
+		self.color = self.colors.get(&id).unwrap_or(&self.background).clone();
 	}
 
 	pub fn define(&mut self, id: u32, color: SIXEL::Color) {
@@ -96,17 +96,6 @@ impl Sixel {
 		self.colors.insert(id, color);
 	}
 
-	pub fn shift(&mut self, amount: u32) {
-		if self.raster.background {
-			for _ in 0 .. amount {
-				self.draw(SIXEL::Map(0));
-			}
-		}
-		else {
-			self.position.0 += amount;
-		}
-	}
-
 	pub fn start(&mut self) {
 		self.position.0 = 0;
 	}
@@ -116,57 +105,58 @@ impl Sixel {
 		self.position.1 += 6 * self.raster.aspect.0;
 	}
 
-	pub fn draw(&mut self, value: SIXEL::Map) {
+	pub fn draw(&mut self, times: u32, value: SIXEL::Map) {
+		// If the value is empty and the background should not be set, it's just a
+		// shift.
 		if !self.raster.background && value.is_empty() {
+			self.position.0 += times;
+			return;
+		}
+
+		for _ in 0 .. times {
+			// The X within the local grid.
+			let x = (self.position.0 / self.cell.0) as usize;
+
+			// Bail out early if the cell is beyond the terminal limit.
+			if x as u32 + self.limit.0 >= self.limit.1 {
+				break;
+			}
+
+			// The X within the image buffer.
+			let xo = self.position.0 % self.cell.0;
+
+			for (i, y) in (self.position.1 .. self.position.1 + (6 * self.raster.aspect.0)).enumerate() {
+				// The bit index within the sixel map.
+				let bit = (i as u32 / self.raster.aspect.0) as u8;
+
+				// The Y within the image buffer.
+				let yo = y as u32 % self.cell.1;
+
+				// The Y within the grid.
+				let y = (y / self.cell.1) as usize;
+
+				// If the grid doesn't have enough rows, extend it.
+				while y >= self.grid.len() {
+					self.grid.push(Vec::new());
+				}
+
+				// If the grid doesn't have enough columns, extend it.
+				while x >= self.grid[y].len() {
+					self.grid[y].push(cairo::Image::new(self.cell.0, self.cell.1));
+				}
+
+				// If the bit is enabled, set it.
+				if value.get(bit) {
+					self.grid[y][x].set(xo, yo, &self.color);
+				}
+				// If disabled bits should set the background color, do so.
+				else if self.raster.background {
+					self.grid[y][x].set(xo, yo, &self.background);
+				}
+			}
+
 			self.position.0 += 1;
-			return;
 		}
-
-		// The color for enabled bits.
-		let color = self.colors.get(&self.color).unwrap_or(&self.background);
-
-		// The X within the local grid.
-		let x = (self.position.0 / self.cell.0) as usize;
-
-		// Bail out early if the cell is beyond the terminal limit.
-		if x as u32 + self.limit.0 >= self.limit.1 {
-			return;
-		}
-
-		// The X within the image buffer.
-		let xo = self.position.0 % self.cell.0;
-
-		for (i, y) in (self.position.1 .. self.position.1 + (6 * self.raster.aspect.0)).enumerate() {
-			// The bit index within the sixel map.
-			let bit = (i as u32 / self.raster.aspect.0) as u8;
-
-			// The Y within the image buffer.
-			let yo = y as u32 % self.cell.1;
-
-			// The Y within the grid.
-			let y = (y / self.cell.1) as usize;
-
-			// If the grid doesn't have enough rows, extend it.
-			while y >= self.grid.len() {
-				self.grid.push(Vec::new());
-			}
-
-			// If the grid doesn't have enough columns, extend it.
-			while x >= self.grid[y].len() {
-				self.grid[y].push(cairo::Image::new(self.cell.0, self.cell.1));
-			}
-
-			// If the bit is enabled, set it.
-			if value.get(bit) {
-				self.grid[y][x].set(xo, yo, color);
-			}
-			// If disabled bits should set the background color, do so.
-			else if self.raster.background {
-				self.grid[y][x].set(xo, yo, &self.background);
-			}
-		}
-
-		self.position.0 += 1;
 	}
 
 	pub fn handle(&mut self, item: &SIXEL::T) {
@@ -184,17 +174,11 @@ impl Sixel {
 			}
 
 			SIXEL::Value(value) => {
-				self.draw(value);
-			}
-
-			SIXEL::Repeat(times, value) if value.is_empty() => {
-				self.shift(times);
+				self.draw(1, value);
 			}
 
 			SIXEL::Repeat(times, value) => {
-				for _ in 0 .. times {
-					self.draw(value);
-				}
+				self.draw(times, value);
 			}
 
 			SIXEL::CarriageReturn => {
